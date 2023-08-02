1565243fba4f41f111c0d9b5c55070896daeef56
Andrew Bennett <andrew@avsoftware.co.uk>
2023-08-02 17:46:10 +0100

    added location data repository to domain module

---
+++ app/location/location_data/src/main/java/uk/co/avsoftware/locationdata/repository/LocationDataRoomRepository.kt
@@ -0,0 +1,12 @@
+package uk.co.avsoftware.locationdata.repository
+
+import kotlinx.coroutines.flow.Flow
+import uk.co.avsoftware.locationdata.dao.LocationDao
+import uk.co.avsoftware.locationdata.entities.Location
+import javax.inject.Inject
+
+class LocationDataRoomRepository @Inject constructor(
+    private val locationDao: LocationDao,
+) {
+    fun getLocationList(): Flow<List<Location>> = locationDao.getAllLocations()
+}
