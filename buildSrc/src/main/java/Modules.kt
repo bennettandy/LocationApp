1781b518f450e9225b3fb080e9e2497d094f5429
Andrew Bennett <andrew@avsoftware.co.uk>
2023-08-10 10:27:54 +0100

    Migrate modules to root (#12)
    * migrated feature modules to project root

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -4,42 +4,42 @@ object Modules {
     const val appNamespace = "uk.co.avsoftware.locationapp"
 
     // Shared Modules
-    const val commonUi = ":app:common-ui"
+    const val commonUi = ":common-ui"
     const val commonUiNamespace = "uk.co.avsoftware.commonui"
 
-    const val common = ":app:common"
+    const val common = ":common"
     const val commonNamespace = "uk.co.avsoftware.common"
 
-    const val commonTest = ":app:common-test"
+    const val commonTest = ":common-test"
     const val commonTestNamespace = "uk.co.avsoftware.commontest"
 
     // Location Feature Modules
-    const val locationDataModule = ":app:location:location_data"
+    const val locationDataModule = ":location:location_data"
     const val locationDataModuleNamespace = "uk.co.avsoftware.locationdata"
 
-    const val locationDomainModule = ":app:location:location_domain"
+    const val locationDomainModule = ":location:location_domain"
     const val locationDomainModuleNamespace = "uk.co.avsoftware.locationdomain"
 
-    const val locationPresentationModule = ":app:location:location_presentation"
+    const val locationPresentationModule = ":location:location_presentation"
     const val locationPresentationModuleNamespace = "uk.co.avsoftware.locationpresentation"
 
     // Space Launch Feature Modules
-    const val spacelaunchDataModule = ":app:spacelaunch:spacelaunch_data"
+    const val spacelaunchDataModule = ":spacelaunch:spacelaunch_data"
     const val spacelaunchDataModuleNamespace = "uk.co.avsoftware.spacelaunchdata"
 
-    const val spacelaunchDomainModule = ":app:spacelaunch:spacelaunch_domain"
+    const val spacelaunchDomainModule = ":spacelaunch:spacelaunch_domain"
     const val spacelaunchDomainModuleNamespace = "uk.co.avsoftware.spacelaunchdomain"
 
-    const val spacelaunchPresentationModule = ":app:spacelaunch:spacelaunch_presentation"
+    const val spacelaunchPresentationModule = ":spacelaunch:spacelaunch_presentation"
     const val spacelaunchPresentationModuleNamespace = "uk.co.avsoftware.spacelaunchpresentation"
 
     // Authentication Module
-    const val authDataModule = ":app:authentication:auth_data"
+    const val authDataModule = ":authentication:auth_data"
     const val authDataModuleNamespace = "uk.co.avsoftware.authdata"
 
-    const val authDomainModule = ":app:authentication:auth_domain"
+    const val authDomainModule = ":authentication:auth_domain"
     const val authDomainModuleNamespace = "uk.co.avsoftware.authdomain"
 
-    const val authPresentationModule = ":app:authentication:auth_presentation"
+    const val authPresentationModule = ":authentication:auth_presentation"
     const val authPresentationModuleNamespace = "uk.co.avsoftware.authpresentation"
 }
